4d511f53c5481e1cb8683af00680c35e25de7e83
Jonathan Brown <jbrown@acuity.social>
2022-04-01 17:28:09 +0700

    Submit setOrder xt.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, inject, onMounted } from 'vue'
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+  web3ListRpcProviders,
+  web3UseRpcProvider
+} from '@polkadot/extension-dapp';
 
 // reactive state
 const count = ref(0)
@@ -9,12 +16,28 @@ function increment() {
   count.value++
 }
 
+let $acuityClient;
+
+async function setOrder() {
+  const allAccounts = await web3Accounts();
+  const address = allAccounts[0].address;
+  const injector = await web3FromAddress(address);
+
+  $acuityClient.api.tx.orderbook
+    .setOrder('0x000000000000000000000000000000000000000000000022', '0x000000000000000000000000000000000000000000000023', 123, 456)
+    .signAndSend(address, { signer: injector.signer }, (status: any) => {
+      console.log(status)
+    });
+}
+
 // lifecycle hooks
-onMounted(() => {
-  console.log(`The initial count is ${count.value}.`)
+onMounted(async () => {
+  $acuityClient = inject('$acuityClient');
+  const allInjected = await web3Enable('Acuity Browser');
 })
 </script>
 
 <template>
   <button @click="increment">Count is: {{ count }}</button>
+  <button @click="setOrder">Set order</button>
 </template>
